a764e9cebdffe27856655a0fe7f7a52f4d23b06f
foldesimiklos <foldesimiklos@gmail.com>
2018-07-22 18:19:38 +0200

    V01

---
+++ TCP2 teszt/Form1.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(402, 333)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Lekérdezés"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(31, 137)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(365, 196)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(31, 339)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 29)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Töröl"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(425, 428)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 37)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Kilép"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1500
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(31, 379)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(93, 21)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "LED_Zöld"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(31, 406)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(96, 21)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "LED_piros"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(31, 432)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(77, 21)
+        Me.CheckBox3.TabIndex = 7
+        Me.CheckBox3.Text = "All_Led"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(14, 25)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(106, 21)
+        Me.RadioButton1.TabIndex = 8
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Local elérés"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Checked = True
+        Me.RadioButton2.Location = New System.Drawing.Point(14, 52)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(110, 21)
+        Me.RadioButton2.TabIndex = 9
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Távoli elérés"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(14, 78)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(75, 21)
+        Me.RadioButton3.TabIndex = 10
+        Me.RadioButton3.Text = "Manual"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(108, 78)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(243, 22)
+        Me.TextBox1.TabIndex = 11
+        Me.TextBox1.Text = "http://192.168.0.26"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 456)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 17)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Kimenő adat"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Din1: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Din2: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 86)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 17)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Din3: "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 115)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 17)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Din4: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 144)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 17)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Analóg:"
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(355, 374)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(175, 21)
+        Me.CheckBox4.TabIndex = 18
+        Me.CheckBox4.Text = "Folyamatos lekérdezés"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(17, 10)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(379, 121)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "IP_webcím"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(15, 173)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 17)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Kapu"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(411, 26)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(117, 251)
+        Me.GroupBox2.TabIndex = 21
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Adatok"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(540, 477)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ TCP2 teszt/Form1.vb
@@ -0,0 +1,231 @@
+﻿Imports System
+Imports System.Net
+Imports System.IO
+Public Class Form1
+    ' ESP8266 webserver lekérdezése HTTP-n keresztül  LEDControlwithWEB_VB_sima.ino Ardunio fileshez igazítva
+    ' 2018.07.08 Teszt sikeres! Miki
+    Dim din As Boolean
+    Dim Ain As Integer
+    Dim valt_zold As Boolean = True
+    Dim valt_piros As Boolean = True
+    Dim led As String
+    Dim sURL As String
+    Dim valtas As Boolean = True
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Timer1.Stop()
+        RadioButton2.Select()
+    End Sub
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.Icon = SystemIcons.Application
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Kapu értesítések"
+            NotifyIcon1.BalloonTipText = "Kapu állapot"
+            NotifyIcon1.ShowBalloonTip(200)
+            'Me.Hide()
+            ShowInTaskbar = False
+            Timer1.Start()
+        End If
+    End Sub
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        'Me.Show()
+        'Timer1.Stop()
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        '  NotifyIcon1.Visible = False
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tcp_elker()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+    End Sub
+    Private Sub tcp_elker()
+        Dim webcim As String
+        webcim = sURL & led
+        Label1.Text = webcim
+        Dim wrGETURL As WebRequest
+        Try
+            wrGETURL = WebRequest.Create(webcim)
+        Catch ex As Exception
+            Timer1.Stop()
+            CheckBox4.Checked = False
+            MsgBox(webcim & "-hoz nem lehet kapcsolódni")
+
+            Exit Sub
+        End Try
+
+
+        Dim myProxy As New WebProxy("myproxy", 80)
+        myProxy.BypassProxyOnLocal = True
+
+        'wrGETURL.Proxy = myProxy
+        ' wrGETURL.Proxy = WebProxy.GetDefaultProxy()
+        Try
+            Dim objStream As Stream
+            objStream = wrGETURL.GetResponse.GetResponseStream()
+
+            Dim objReader As New StreamReader(objStream)
+            Dim sLine As String = ""
+            Dim i As Integer = 0
+
+            Do While Not sLine Is Nothing
+                i += 1
+                sLine = objReader.ReadLine
+                If Not sLine Is Nothing Then
+                    ListBox1.Items.Add(i & " " & sLine)
+                    Select Case i
+                        Case 2
+                            If sLine = "0" Then
+                                Label2.Text = "Din1: Low"
+                            ElseIf sLine = "1" Then
+                                Label2.Text = "Din1: High"
+                            End If
+                        Case 3
+                            If sLine = "0" Then
+                                Label3.Text = "Din2: Low"
+                            ElseIf sLine = "1" Then
+                                Label3.Text = "Din2: High"
+                            End If
+                        Case 4
+                            If sLine = "0" Then
+                                Label4.Text = "Din3: Low"
+                            ElseIf sLine = "1" Then
+                                Label4.Text = "Din3: High"
+                            End If
+                        Case 5
+                            If sLine = "0" Then
+                                din = False
+                                Label5.Text = "Din4: Low"
+                            ElseIf sLine = "1" Then
+                                Label5.Text = "Din4: High"
+                                din = True
+                            End If
+                        Case 7
+                            Ain = Val(sLine)
+                            Label6.Text = "Analóg: " & Ain
+                    End Select
+
+                End If
+            Loop
+        Catch ex As Exception
+            MsgBox("Nem lehet kapcsolódni a kiszolgálóhoz")
+            Timer1.Stop()
+            CheckBox4.Checked = False
+        End Try
+
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        tcp_elker()
+
+        If din = Not valtas Then
+
+            NotifyIcon1.Visible = False
+            If din = True Then
+
+                NotifyIcon1.Icon = My.Resources.zöld
+                NotifyIcon1.BalloonTipText = "Nyitás"
+                Label7.Text = "Kapu Nyit"
+                ' NotifyIcon1.ShowBalloonTip(500)
+                NotifyIcon1.Text = "Nyitás"
+            Else
+                NotifyIcon1.Icon = My.Resources.piros
+                NotifyIcon1.BalloonTipText = "Zárás"
+                Label7.Text = "Kapu Zár"
+                NotifyIcon1.Text = "Zárás"
+            End If
+            NotifyIcon1.Visible = True
+            NotifyIcon1.ShowBalloonTip(500)
+        End If
+        valtas = din
+    End Sub
+
+
+    Protected Sub PostTo(url As String, postData As String)
+        'Dim cim As String = "http://192.168.0.26"
+        ' PostTo(cim, "szöveg")
+
+        Dim myWebRequest As HttpWebRequest = TryCast(WebRequest.Create(url), HttpWebRequest)
+        myWebRequest.Method = "POST"
+        Dim byteArray As Byte() = System.Text.Encoding.[Default].GetBytes(postData)
+        myWebRequest.ContentType = "application/x-www-form-urlencoded"
+        myWebRequest.ContentLength = byteArray.Length
+        Dim dataStream As System.IO.Stream = myWebRequest.GetRequestStream()
+        dataStream.Write(byteArray, 0, byteArray.Length)
+        dataStream.Close()
+
+        'Dim myWebResponse As WebResponse = myWebRequest.GetResponse()
+        'myWebResponse.Write((DirectCast(myWebResponse, HttpWebResponse)).StatusDescription)
+        ' dataStream = myWebResponse.GetResponseStream()
+        ' Dim reader As New System.IO.StreamReader(dataStream)
+        ' Dim responseFromServer As String = reader.ReadToEnd()
+        ' Response.Write(responseFromServer + ":")
+        'reader.Close()
+        ' dataStream.Close()
+        'myWebResponse.Close()
+
+    End Sub
+
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            led = "/LED1=ON"
+        Else
+            led = "/LED1=OFF"
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            led = "/LED2=ON"
+        Else
+            led = "/LED2=OFF"
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            led = "/ALL=ON"
+        Else
+            led = "/ALL=OFF"
+        End If
+    End Sub
+
+
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        sURL = "http://188.36.38.165:1418"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        sURL = "http://192.168.0.26"
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+        If TextBox1.Text.Trim.Length > 0 Then
+            sURL = TextBox1.Text
+        Else
+            MsgBox("No String specified")
+        End If
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.Checked = True Then
+            Timer1.Start()
+        Else
+            Timer1.Stop()
+        End If
+    End Sub
+
+
+End Class
